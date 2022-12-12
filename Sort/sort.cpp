8980d9117c373a704351582a3ec2d03f4a1e0c97
BitaminW <114838001+BitaminW@users.noreply.github.com>
2022-12-13 05:10:20 +0900

    Update sort.cpp

--- Sort/sort.cpp
+++ Sort/sort.cpp
@@ -54,3 +54,66 @@ void insertion_sort(int* arr, int arr_len) {
 		arr[j + 1] = temp;
 	}
 }
+
+
+void merge(int arr[],const int left,const int mid,const int right) {
+	
+	const auto subArrayOne = mid - left + 1;
+	const auto subArrayTwo = right - mid;
+
+	auto* leftArray  = new int[subArrayOne],
+		* rightArray = new int[subArrayTwo];
+
+	for(auto i = 0; i < subArrayOne; i++) {
+		leftArray[i] = arr[left + i];
+	}
+	for (auto j = 0; j < subArrayTwo; j++) {
+		rightArray[j] = arr[mid + 1 + j];
+	}
+	
+	auto indexOfSubArrayOne = 0,
+		 indexOfSubArrayTwo = 0;
+	int indexOfMergedArray = left;
+
+	while (indexOfSubArrayOne < subArrayOne
+		&& indexOfSubArrayTwo < subArrayTwo) {
+		
+		if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+			arr[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+			indexOfSubArrayOne++;
+		}
+		else {
+			arr[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+			indexOfSubArrayTwo++;
+		}
+		indexOfMergedArray++;
+	}
+
+	while (indexOfSubArrayOne < subArrayOne) {
+		arr[indexOfMergedArray]
+			= leftArray[indexOfSubArrayOne];
+		indexOfSubArrayOne++;
+		indexOfMergedArray++;
+	}
+	
+	while (indexOfSubArrayTwo < subArrayTwo) {
+		arr[indexOfMergedArray]
+			= rightArray[indexOfSubArrayTwo];
+		indexOfSubArrayTwo++;
+		indexOfMergedArray++;
+	}
+	delete[] leftArray;
+	delete[] rightArray;
+
+}
+
+void merge_sort(int array[], int const begin, int const end)
+{
+	if (begin >= end)
+		return; // Returns recursively
+
+	auto mid = begin + (end - begin) / 2;
+	merge_sort(array, begin, mid);
+	merge_sort(array, mid + 1, end);
+	merge(array, begin, mid, end);
+}
