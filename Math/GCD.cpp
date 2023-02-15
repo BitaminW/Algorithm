971919191d82164c05807c9696b702c981374ef9
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-02-15 22:21:42 +0900

    Update GCD.cpp

--- Math/GCD.cpp
+++ Math/GCD.cpp
@@ -1,1 +1,25 @@
+int gcd1(int x, int y) {
+	if (y == 0) {
+		return x;
+	}
+	else {
+		return gcd1(y, x % y);
+	}
+}
 
+int gcd2(int x, int y) {
+	int temp;
+	if (x < y) {
+		temp = x;
+		x = y;
+		y = temp;
+	}
+	int rest;
+
+	while (y != 0) {
+		rest = x % y;
+		x = y;
+		y = rest;
+	}
+	return x;
+}
